8801d5247a9590efc7ea9fe488e8bc008652a033
Lucas Fontinele <contato@fontinele.dev>
2024-03-25 15:24:00 -0300

    chore: implements layout

--- src/presentation/navigation/components/Layout/index.tsx
+++ src/presentation/navigation/components/Layout/index.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "../../../../shared/components/Header"
+import styles from "./styles.module.scss"
 
 export function Layout() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
 
       <Outlet />
